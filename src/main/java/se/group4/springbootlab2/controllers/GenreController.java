e6d03ac78481b58f5f741640487506633f013f34
CooBoo84 <kennie_svensson@hotmail.com>
2021-02-26 10:27:09 +0100

    Updated .pom with xml-dependency. Updated @RestController with overall /genres link

--- src/main/java/se/group4/springbootlab2/controllers/GenreController.java
+++ src/main/java/se/group4/springbootlab2/controllers/GenreController.java
@@ -8,7 +8,7 @@ import se.group4.springbootlab2.services.GenreService;
 
 import java.util.List;
 
-@RestController
+@RestController("/genres")                                                                                              //Övergripande länk
 public class GenreController {                                                                                          //Tar hand om inkommande anrop
 
     private GenreService genreService;
@@ -17,12 +17,12 @@ public class GenreController {                                                  
         this.genreService = genreService;
     }
 
-    @GetMapping("/genres")
+    @GetMapping()
     public List<GenreDto> all(){
         return genreService.getAllGenres();
     }
 
-    @GetMapping("/genres/{id}")
+    @GetMapping("/{id}")
     public GenreDto one(@PathVariable int id){                                                                          //@PathVariable plockar ett värde från url-path
         return genreService.getOne(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Id " + id + " not found."));      // Returnerar om Genre finns, annars en exception
@@ -32,24 +32,24 @@ public class GenreController {                                                  
     }
 
 
-    @PostMapping("/genres")
+    @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)                                                                                 //HttpStatus.CREATED - Ändrar kod till 201
     public GenreDto create(@RequestBody GenreDto genre){                                                                      //@RequestBody plockar info från json body
       return genreService.createGenre(genre);
     }
 
 
-    @DeleteMapping("/genres/{id}")
+    @DeleteMapping("/{id}")
     public void delete(@PathVariable int id){
         genreService.delete(id);
     }
 
-    @PutMapping("/genres/{id}")                                                                                       //Uppdaterar hela objektet
+    @PutMapping("/{id}")                                                                                       //Uppdaterar hela objektet
     public GenreDto replace(@RequestBody GenreDto genreDto, @PathVariable int id){
        return genreService.replace(id, genreDto);
     }
 
-    @PatchMapping("/genres/{id}")                                                                                     //Uppdaterar en variabel (typ bara Email). Om man lägger till fler variabler, skapa fler klasser, se 20210219_111245 29:27
+    @PatchMapping("/{id}")                                                                                     //Uppdaterar en variabel (typ bara Email). Om man lägger till fler variabler, skapa fler klasser, se 20210219_111245 29:27
     public GenreDto update(@RequestBody GenreDto genreDto, @PathVariable int id){
         return genreService.update(id, genreDto);
     }
